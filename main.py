d63d70327852613fcbdd0cfb6d2b261832e9d4af
harsh-prasad <130177302+HARSH2815@users.noreply.github.com>
2026-04-19 16:07:55 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,8 +54,7 @@ movies.columns = movies.columns.str.replace(' ', '_')
 
 @st.cache_data
 def load_similarity():
-    url="https://drive.google.com/file/d/1gZR5ix4q66N3mqKfIAnOuBaK-iQI1d77/view?usp=drive_link"
-    # url = "https://drive.google.com/uc?id=1wEfFlp0aJiPuzjmNzxr2GomCQ0Zl8N1_"
+    url="https://drive.google.com/uc?id=1gZR5ix4q66N3mqKfIAnOuBaK-iQI1d77"
     output = "similarity.pkl"
     gdown.download(url, output, quiet=False)
     return pickle.load(open(output, 'rb'))
